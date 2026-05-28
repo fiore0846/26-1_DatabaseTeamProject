c7fefb7d46fc0d5bf90adfc2c0fb5439bb326236
fiore0846 <fiore08460@gmail.com>
2026-05-28 15:43:29 +0900

    Create Customer.java

---
+++ src/db_2026_team06/model/Customer.java
@@ -0,0 +1,30 @@
+package DB2026Team06.model;
+
+public class Customer {
+	private int customer_id;
+	private String name;
+	private String email;
+	private String phone;
+	
+	//생성자
+	public Customer() {}
+	public Customer(int customer_id, String name, String email, String phone) {
+		this.customer_id = customer_id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+	
+	//getter, setter
+	public int getCustomerId()               { return customer_id; }
+	public void setCustomerId(int customerId){ this.customer_id = customerId; }
+	
+	public String getName()              { return name; }
+	public void setName(String name)     { this.name = name; }
+	
+	public String getEmail()             { return email; }
+	public void setEmail(String email)   { this.email = email; }
+	
+	public String getPhone()             { return phone; }
+	public void setPhone(String phone)   { this.phone = phone; }
+}
